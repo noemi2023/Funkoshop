858f267d57a714d139087ff3ff9aecc82965a23d
noemi2023 <noemibonasera@yahoo.com>
2023-10-16 20:46:48 -0700

    estructura inicial del sitio funkoshop-grupo7

---
+++ Formulario.php
@@ -0,0 +1,143 @@
+
+<!DOCTYPE HTML>
+<html lang="es">
+	<head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>FUNKOSHOP</title>
+		<link rel="stylesheet" href="style.css" />
+	</head>
+	<body>
+		<header>
+            <h1>FUNKOSHOP</h1>
+	<img src="/image/funkos-banner.wepp" alt="Banner">
+			<nav id="nav">
+				<ul>
+							<li><a href="#">Shop</a></li>
+							<li><a href="#">Contacto</a></li>
+							<li><a href="#">Login</a></li>
+									</li>
+									</ul>
+									</nav>
+								</header>
+								<section id="main">
+									<div class="container">
+										<div class="row">
+						<div class="row aln-center">
+									<section>
+										<article>
+                                        <div class="row">
+    <div class="col-6 col-12-medium">
+        <section>
+            <form method="post" action="#">
+                <div class="row gtr-50">
+                    <div class="col-6 col-12-small">
+                        <input name="name" placeholder="Name" type="text" />
+                    </div>
+                    <div class="col-6 col-12-small">
+                        <input name="email" placeholder="Email" type="text" />
+                    </div>
+                    <div class="col-12">
+                        <textarea name="message" placeholder="Message"></textarea>
+                    </div>
+                    <div class="col-12">
+                        <a href="#" class="form-button-submit button icon solid fa-envelope">Send Message</a>
+                    </div>
+                </div>
+            </form>
+        </section>
+    </div>
+										</article>
+									</section>
+									</div>
+								</section>
+<aside class="sidebar" id="sidebar izquierdo">
+			<h4>BUSCAR</h4>
+			<form class="js-search-form search-form " action="/search/" method="get">
+				<div class="form-group m-0">
+				<input class="js-search-input form-control search-input background-secondary" autocomplete="off" type="search" name="q" placeholder="¿Qué estás buscando?" aria-label="Item o categoria" />
+				<button type="submit" class="js-search-input-submit search-btn search-submit-btn mr-2" value="Buscar" aria-label="Buscar">
+				</button>
+				<a href="#" class="js-empty-search search-btn search-empty-btn " style="display: none;">
+				</a>
+				</div>
+				</form>
+				<h4>ORDENAR POR</h4>
+				<form class="js-search-form search-form " action="/search/" method="get">
+					<div class="form-group m-0">
+					<input class="js-search-input form-control search-input background-secondary" autocomplete="off" type="search" name="q" placeholder="¿Qué estás buscando?" aria-label="Mayo a menor" />
+					<button type="submit" class="js-search-input-submit search-btn search-submit-btn mr-2" value="Buscar" aria-label="Buscar">
+					</button>
+					<a href="#" class="js-empty-search search-btn search-empty-btn " style="display: none;">
+					</a>
+					</div>
+					</form>
+					<h4>PRECIO</h4>
+					<H4>FILTRAR</H4>
+					</aside>
+				
+			
+			<!-- Footer -->
+				<section id="footer">
+					<div class="container">
+						<h2>Contactenos</h2>
+						
+							<div class="col-6 col-12-medium">
+								<section>
+									<p>Erat lorem ipsum veroeros consequat magna tempus lorem ipsum consequat Phaselamet
+									mollis tortor congue. Sed quis mauris sit amet magna accumsan tristique. Curabitur
+									leo nibh, rutrum eu malesuada.</p>
+									<div class="row">
+										<div class="col-6 col-12-small">
+											<ul class="icons">
+												<li class="icon solid fa-home">
+													1234 Somewhere Road<br />
+													Nashville, TN 00000<br />
+													USA
+												</li>
+												<li class="icon solid fa-phone">
+													(000) 000-0000
+												</li>
+												<li class="icon solid fa-envelope">
+													<a href="#">info@untitled.tld</a>
+												</li>
+											</ul>
+										</div>
+										<div class="col-6 col-12-small">
+											<ul class="icons">
+												<li class="icon brands fa-twitter">
+													<a href="#">@untitled</a>
+												</li>
+												<li class="icon brands fa-instagram">
+													<a href="#">instagram.com/untitled</a>
+												</li>
+												<li class="icon brands fa-dribbble">
+													<a href="#">dribbble.com/untitled</a>
+												</li>
+												<li class="icon brands fa-facebook-f">
+													<a href="#">facebook.com/untitled</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</section>
+							</div>
+						</div>
+					</div>
+					<div id="copyright" class="container">
+						<ul class="links">
+							<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						</ul>
+					</div>
+				</section>
+
+		</div>
+
+		
+	</body>
+</html>
+
+
+
+
+
